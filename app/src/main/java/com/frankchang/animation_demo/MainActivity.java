30f04a295d8f37f08ae46d3a6853f652c47a4aa9
lus33frank <lus33frank@gmail.com>
2018-10-27 16:40:34 +0800

    Commit : 2    專案練習完成。 圖片變換動畫

--- app/src/main/java/com/frankchang/animation_demo/MainActivity.java
+++ app/src/main/java/com/frankchang/animation_demo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.frankchang.animation_demo;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView show;
+    private AnimationDrawable animation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        show = findViewById(R.id.showImage);
+        animation = (AnimationDrawable) show.getDrawable();
     }
+
+    public void startOnClick(View view) {
+        animation.start();
+    }
+
+    public void stopOnClick(View view) {
+        animation.stop();
+    }
+
 }
